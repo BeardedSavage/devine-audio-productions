740b4c22175a5dbb338802cf20d70719d63b169b
BeardedSavage <dangelidakis2015@gmail.com>
2025-09-05 12:23:42 -0500

    Udated npm module again:

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables from .env files
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins:[react()],
-    server: {
-      // Use the PORT from the .env file, or fall back to a default value
-      port: env.PORT || 5173,
-    },
-    // ... other config
-  };
+export default defineConfig({
+   plugins:[react()]
 });
